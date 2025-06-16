7694892a25f4f8272058081cb7dbe4b808b3cd63
Oscar <oscar.cornut@gmail.com>
2025-06-16 12:29:29 +0200

    feat: add flake.nix and remove unused files

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Environnement Python avec tqdm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        pythonEnv =
+          pkgs.python312.withPackages (ps: with ps; [ tqdm requests numpy ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pythonEnv ];
+          shellHook = ''
+            exec ${pkgs.fish}/bin/fish
+          '';
+        };
+
+      });
+}
