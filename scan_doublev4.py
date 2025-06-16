fc32d5f1a2dac68dc454d6f6d94de833804f188d
Oscar <oscar.cornut@gmail.com>
2025-06-16 10:57:50 +0200

    fix: add size row on the csv output for the sort

--- scan_doublev4.py
+++ scan_doublev4.py
@@ -146,14 +146,14 @@ def export_csv(doublons: dict, output_file: str = "output.csv"):
     try:
         with open(output_file, mode="w", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow(["Doublon", "Original", "Taille (octets)"])
+            writer.writerow(["Doublon", "Original", "Taille (humain), Taille (Tri)"])
             for doublon, original in doublons.items():
                 try:
                     size = os.path.getsize(doublon)
                 except Exception as e:
                     print(f"Erreur pour obtenir la taille de {doublon} : {e}")
                     size = "?"
-                writer.writerow([doublon, original, print_size(size)])
+                writer.writerow([doublon, original, print_size(size)], size)
         print(f"Fichier CSV généré : {output_file}")
     except Exception as e:
         print(f"Erreur lors de la génération du fichier CSV : {e}")
